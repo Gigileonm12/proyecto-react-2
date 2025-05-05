7117d4b3340f2c915876532f918fe090d6738b9f
Gigileonm12 <ghislaineleonmolina@gmail.com>
2025-05-05 00:55:04 -0500

    act2

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import React, { useEffect, useContext } from 'react';
-import { ConfigProvider, theme as antdTheme } from 'antd';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { ThemeProvider } from './contexts/ThemeContext';
+import {  AuthProvider } from './contexts/AuthContext';
 import LoginForm from './components/LoginForm';
+import { ConfigProvider, theme as antdTheme  } from 'antd';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import PrincipalPage from './pages/PrincipalPage';
-import { AuthContext } from './contexts/AuthContext';
 
 const theme = 'light'; // o 'dark'
+const isAuthenticated = true; // o false
 
 function AppContent() {
-  const { isAuthenticated } = useContext(AuthContext);
-
-  useEffect(() => {
-    document.body.classList.remove('light-theme', 'dark-theme');
-    document.body.classList.add(theme === 'light' ? 'light-theme' : 'dark-theme');
-  }, []);
+useEffect(() => {
+  document.body.classList.remove('light-theme', 'dark-theme');
+  document.body.classList.add(theme === 'light' ? 'light-theme' : 'dark-theme');
+}, []);
 
   return (
     <ConfigProvider
@@ -23,10 +23,22 @@ function AppContent() {
     >
       <Router>
         <Routes>
-          <Route path="/" element={isAuthenticated ? <Navigate to="/principal" /> : <LoginForm />} />
+          <Route path="/" element={isAuthenticated ? <Navigate to="/principal" /> : <LoginForm />} /> 
           <Route path="/principal/*" element={isAuthenticated ? <PrincipalPage /> : <Navigate to="/" />} />
         </Routes>
       </Router>
     </ConfigProvider>
   );
 }
+
+function App() {
+  return (
+    <ThemeProvider>  
+      <AuthProvider> 
+        <AppContent />
+      </AuthProvider>
+    </ThemeProvider>
+  );
+}
+
+export default App;
